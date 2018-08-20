fb193993f1c57d8c43cb48f34bc479688913f602
kodakodu <kodakodu@gmail.com>
2018-08-20 12:22:09 +0200

    fin video

--- react-sql/src/App.js
+++ react-sql/src/App.js
@@ -5,7 +5,11 @@ import './App.css';
 class App extends Component {
 
   state = {
-    products:[]
+    products:[],
+    product : {
+      name: 'sample product',
+      price:20
+    }
   }
 
   componentDidMount(){
@@ -19,10 +23,17 @@ class App extends Component {
     .catch(err => console.error(err))
   }
 
+  addProduct = _ =>{
+    const {product}= this.state;
+    fetch(`http://localhost:4000/products/add?name=${product.name}&price=${product.price}`)
+    .then(this.getProducts)
+    .catch(err => console.error(err))
+  }
+
   renderProduct = ({product_id,name}) => <div key={product_id}>{name}</div>
 
   render() {
-    const {products}= this.state;
+    const {products,product}= this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -31,6 +42,13 @@ class App extends Component {
         <p className="App-intro">
           {products.map(this.renderProduct)}
         </p>
+        <input 
+          value={product.name} 
+          onChange={e => this.setState({ product: {...product, name: e.target.value}})}/>
+        <input 
+          value={product.price}
+          onChange={e => this.setState({ product: {...product, price: e.target.value}})}/>
+          <button onClick={this.addProduct}>Add product</button>
       </div>
     );
   }
